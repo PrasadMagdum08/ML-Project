fe1eb15e8ab48271ca1576789fd81694340c2c09
PrasadMagdum08 <magdumprasad194@gmail.com>
2024-12-01 15:40:24 +0530

    Updated utils

--- src/ML_Project/utils.py
+++ src/ML_Project/utils.py
@@ -43,3 +43,19 @@ def read_sql_data():
     
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def save_object(file_path, obj):
+
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        # print(traceback.format_exc())
+        raise CustomException(e, sys)
+    
